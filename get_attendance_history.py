eaddc0024e68926ba2df450027c1e9a8d7e86add
eslam magdi <eslam.magdi.fcis@gmail.com>
2020-12-14 22:52:39 +0200

    Added get_attendance_history for make report about employee working days

---
+++ get_attendance_history.py
@@ -0,0 +1,42 @@
+import json
+
+from connect_database import execute_query
+from uitls import datetime_utc_format, format_date, format_datetime
+
+DATABASE_PATH = "data/attendance.db"
+
+def get_attendance_history(empylee_code):
+
+    sql_query = """
+                SELECT 
+                    a.day, 
+                    actions.Action, 
+                    actions.ActionTime
+                FROM 
+                    Attendance a
+                    INNER JOIN AttendanceActions actions ON  a.Id = actions.AttendanceId
+                WHERE
+                    lower(a.employee) = '{empylee_code}'
+                ORDER BY
+	                actions.Id  
+            """.format(empylee_code=empylee_code.lower())
+    
+    result = execute_query(databse_path=DATABASE_PATH, sql_query=sql_query)
+
+    days = []
+    days_set = sorted(set([day for day, _, _ in result]))
+
+    for day_set in days_set:
+        actions = []
+
+        for _, action, action_time in result:
+            action_time = format_datetime(action_time)
+            action_time = datetime_utc_format(action_time)
+
+            if format_date(day_set) == action_time.date():
+                actions.append({"action": action, "time": action_time.isoformat() })
+
+
+        days.append({"date": day_set, "actions": actions})
+
+    return json.dumps({"days": days})
